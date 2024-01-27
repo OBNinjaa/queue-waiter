85fa196fe72113c8dd50c8a2842f75c278a21391
OBNinjaa <64389208+OBNinjaa@users.noreply.github.com>
2024-01-27 17:35:09 +0000

    purge unloaded packets

--- src/proxy.js
+++ src/proxy.js
@@ -1,98 +1,105 @@
-const { createClient, createServer, states } = require("minecraft-protocol");
+const { createServer, createClient, states } = require("minecraft-protocol");
+const { username, version, port } = require("./settings.json");
 
-let userClient;
-const packets = [];
-let previousQueueNumber = null;
+const server = createServer({ "online-mode": false, port, host: "0.0.0.0", version, keepAlive: false, hideErrors: true });
+const remote = createClient({ host: "2B2T.ORG", port: 25565, version, username, auth: "microsoft", hideErrors: true });
 
-const { username } = require("./username.json");
+const packets = new Map();
+let user;
+let queue;
 
-const proxyClient = createClient({
-  username: username,
-  auth: "microsoft",
-  host: "2B2T.ORG",
-  port: 25565,
-  keepAlive: true,
-  version: "1.19.4",
-  hideErrors: true,
+remote.on("packet", handleRemotePacket);
+server.on("login", handleClientLogin);
+
+remote.on("end", () => {
+  if (!user) return;
+  user.end(`Something has gone terribly wrong.`);
+  console.log(`[!] The remote client has been disconnected.`);
 });
 
-proxyClient.on("packet", (data, meta) => {
-  if (meta.name === "keep_alive") return;
-  if (!["keep_alive", "success", "custom_payload", "encryption_begin", "compress", "look", "flying", "open_window", "close_window", "close_window", "player_chat", "profileless_chat"].includes(meta.name)) packets.push([meta, data]);
+function purgeChunk(x, z) {
+  const keysToRemove = [];
 
-  if (meta.name === "set_title_subtitle") {
-    try {
-      const parsedData = JSON.parse(data.text);
-      const queueText = parsedData.text;
-      const queueNumber = parseInt(queueText.match(/\d+/)[0]);
+  packets.forEach((data, meta) => {
+    if (meta.name === "map_chunk" && data.x === x && data.z === z) {
+      keysToRemove.push(meta);
+    }
+  });
 
-      if (previousQueueNumber !== queueNumber) {
-        const currentTime = new Date().toLocaleTimeString(); // Get the current time
-        console.log(`[${currentTime}] Position in queue: ${queueNumber}`); // Display time with queue position
-        previousQueueNumber = queueNumber;
-      }
-    } catch (err) {
-      return;
+  keysToRemove.forEach((key) => {
+    packets.delete(key);
+  });
+}
+
+function handleRemotePacket(data, meta) {
+  if (shouldIgnorePacket(meta.name)) return;
+
+  if (!dontStore(meta.name)) {
+    packets.set(meta, data);
+  }
+
+  if (meta.name === "unload_chunk") {
+    purgeChunk(data.chunkX, data.chunkZ);
+  }
+
+  if ([("title", "set_title_subtitle")].includes(meta.name)) {
+    const parsedData = JSON.parse(data.text);
+    const queueText = parsedData.text;
+    const queueNumber = parseInt(queueText.match(/\d+/)[0]);
+
+    if (queue !== queueNumber) {
+      const currentTime = new Date().toLocaleTimeString();
+      console.log(`[${currentTime}] Position in queue: ${queueNumber}`);
+      queue = queueNumber;
     }
   }
 
-  if (!userClient || meta.state !== states.PLAY || userClient.state !== states.PLAY) return;
-  userClient.write(meta.name, data);
-  if (meta.name === "set_compression") userClient.compressionThreshold = data.threshold;
-});
+  if (user && meta.state === states.PLAY && user.state === states.PLAY) {
+    handleUserPacket(meta, data);
+  }
+}
 
-proxyClient.on("raw", (buffer, meta) => {
-  if (!userClient || meta.name === "keep_alive" || meta.state !== states.PLAY || userClient.state !== states.PLAY) return;
-});
+function handleUserPacket(meta, data) {
+  if (meta.name === "set_compression") {
+    user.compressionThreshold = data.threshold;
+  }
 
-proxyClient.on("end", () => {
-  if (!userClient) return;
-  userClient.end("Proxy client ended");
-});
+  user.write(meta.name, data);
+}
 
-proxyClient.on("error", (error) => {
-  if (!userClient) return;
-  console.error("Proxy client error:", error);
-  userClient.end(error);
-});
+function handleClientLogin(client) {
+  console.log(`[+] ${client.username} connected to the proxy server.`);
 
-const proxyServer = createServer({
-  "online-mode": true,
-  host: "0.0.0.0",
-  port: 25566,
-  keepAlive: false,
-  version: "1.19.4",
-});
+  if (client.state === states.PLAY) {
+    packets.forEach((data, meta) => {
+      if (!shouldIgnorePacket(meta.name)) {
+        client.write(meta.name, data);
+      }
+    });
 
-proxyServer.on("login", (client) => {
-  console.log(`${client.username} has connected`);
+    user = client;
 
-  packets.forEach((p) => {
-    const meta = p[0];
-    const data = p[1];
-    client.write(meta.name, data);
-  });
+    client.on("end", () => {
+      console.log(`[-] ${client.username} disconnected from the proxy server.`);
+    });
+  }
 
-  userClient = client;
+  client.on("packet", handleClientPacket);
+}
 
-  client.on("packet", (data, meta) => {
-    if (meta.name === "keep_alive") return;
-    if (!proxyClient || meta.state !== states.PLAY || proxyClient.state !== states.PLAY) return;
-    proxyClient.write(meta.name, data);
-  });
+function handleClientPacket(data, meta) {
+  if (shouldIgnorePacket(meta.name)) return;
+  if (!remote || meta.state !== states.PLAY || remote.state !== states.PLAY) return;
 
-  client.on("raw", (buffer, meta) => {
-    if (meta.name === "keep_alive") return;
-    if (!proxyClient || meta.state !== states.PLAY || proxyClient.state !== states.PLAY) return;
-  });
+  remote.write(meta.name, data);
+}
 
-  client.on("end", () => {
-    if (!proxyClient) return;
-    console.log(`${client.username} has disconnected`);
-  });
+function shouldIgnorePacket(packetName) {
+  const ignoredPackets = ["keep_alive", "success", "custom_payload", "encryption_begin", "compress", "registry_data", "finish_configuration", "server_data"];
+  return ignoredPackets.includes(packetName);
+}
 
-  client.on("error", (error) => {
-    if (!proxyClient) return;
-    console.error(error);
-  });
-});
+function dontStore(packetName) {
+  const ignoredPackets = ["title", "set_title_subtitle", "unload_chunk", "chat"];
+  return ignoredPackets.includes(packetName);
+}
